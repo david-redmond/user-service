c0e4cb3d6ea33070ec3c097c5b3d1c84bd83dffe
david-redmond <mail.davidredmond@gmail.com>
2024-03-25 11:10:10 +0000

    added linting

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,18 +2,19 @@ import * as process from "process";
 import * as mongoose from "mongoose";
 
 const mongoConnection = () => {
-    try {
-        const mongoDbURL = process.env.MONGODB_URI || "missing mongodb-uri";
+  try {
+    const mongoDbURL = process.env.MONGODB_URI || "missing mongodb-uri";
 
-        mongoose.connect(mongoDbURL, {useNewUrlParser: true, useUnifiedTopology: true} as any);
-        const db = mongoose.connection;
-        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-        db.once('open', () => console.log('Connected to MongoDB'));
-    } catch (e) {
-        console.error("Error mongoConnection: ", e);
-    }
+    mongoose.connect(mongoDbURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as any);
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+    db.once("open", () => console.log("Connected to MongoDB"));
+  } catch (e) {
+    console.error("Error mongoConnection: ", e);
+  }
 };
 
-export {
-    mongoConnection
-};
+export { mongoConnection };
